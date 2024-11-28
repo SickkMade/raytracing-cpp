bc116c63d48cc302db617d62c870acef73cef03d
sickk <jaakmyr@gmail.com>
2024-11-28 10:42:57 -0800

    added progress bar

--- main.cpp
+++ main.cpp
@@ -9,8 +9,10 @@ int main(){
     //top of image for ppm settings
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for(int i = 0; i < image_height; i++){
-        for(int j = 0; j < image_width; j++){
+    for(int j = 0; j < image_height; j++){
+        std::clog << "\rScanlines remaining" << (image_height - j) << ' ' <<  std::flush;
+        for(int i = 0; i < image_width; i++){
+            //define rgb value from 0-255 / 255 (0-1) for each pixel between vertexes
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
             auto b = 0.0;

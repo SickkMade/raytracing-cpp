0f8e507462ce7b3cd4d2580ffbc917670c23e162
sickk <jaakmyr@gmail.com>
2024-11-30 12:40:43 -0800

    using hit i calculate the noramls and shade accordinly

--- main.cpp
+++ main.cpp
@@ -4,23 +4,30 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    
+    if(discriminant < 0){
+        return -1;
+    } else {
+        //quadratic formula
+        return (-b - std::sqrt(discriminant) ) / (2.0*a);
+    }
 }
 
 color ray_color(const ray& r){
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if(t>=0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1)); //normal
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
-    auto b = 0.5*(unit_direction.z() + 1.0);
-    
-    if(hit_sphere(point3(0,0,-1), 0.5, r))
-        return (1.0-b)*color(1.0, 1.0, 1.0) + 10 * b *color(0.2, 0.7, 1.0);
-
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
